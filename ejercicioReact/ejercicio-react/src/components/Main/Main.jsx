6cac3e461ad42a31ee29222b7897e02bbb1b510c
Cesario87 <ces.albred@gmail.com>
2023-02-15 09:03:26 +0100

    test

---
+++ ejercicioReact/ejercicio-react/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import InfoList from './InfoList'
+
+class Main extends Component {
+
+  render() {
+    return <main>
+      <InfoList/>
+    </main>;
+  }
+}
+
+export default Main;
