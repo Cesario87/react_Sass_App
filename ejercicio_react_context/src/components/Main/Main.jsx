186b0264e50aac2019ced691eaa8fdc2c9d9a118
Cesario87 <ces.albred@gmail.com>
2023-02-20 16:47:29 +0100

    1st commit

---
+++ ejercicio_react_context/src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Card from './Card';
+import Form from './Form';
+
+class Main extends Component {
+  render() {
+    return <div>
+      <Form />
+      <Card />
+    </div>;
+  }
+}
+
+export default Main;
